b0ce36864dca6063bd8dd664939a79d21113b9ff
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-03-28 14:36:52 -0600

    refactored table

---
+++ src/components/common/like.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Like({liked, onClick}) {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+}
+
